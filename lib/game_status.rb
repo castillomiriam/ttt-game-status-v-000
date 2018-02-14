4de4bc7b5420b6cd015a713da3abaf35bf9c7728
Miriam Castillo <castillo.m.mc@gmail.com>
2018-02-14 05:23:22 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -50,7 +50,7 @@ end
 def winner(board)
   winning_array = won?(board)
   winning_index = winning_array[0]
-  if winning index == "X"
+  if winning_index == "X"
     return "X"
   else
     return "O"
